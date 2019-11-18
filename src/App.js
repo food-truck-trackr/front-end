ac9aa5dd4d9808797ac60398796ff3e6bb1490a0
Rasha Moumneh <rasha@fastmail.com>
2019-11-18 00:18:52 -0500

    Create customer rating component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import CustomerRating from "./components/diner/CustomerRating";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CustomerRating />
+    </div>
+  );
 }
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
 // Library
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+// import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+// import thunk from "redux-thunk";
 
-// Reducers
-const combinedReducers = combineReducers();
+// // Reducers
+// const combinedReducers = combineReducers();
 
-const store = createStore(
-  combinedReducers,
-  {},
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// const store = createStore(
+//   combinedReducers,
+//   {},
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
-export default store;
+// export default store;
