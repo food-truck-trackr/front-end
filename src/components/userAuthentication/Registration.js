430918c5225ca832354ea67ea318621ef79574ab
wchamber01 <williamchambers@gmail.com>
2019-11-19 19:09:44 -0500

    working on password validation and clearing 500 error on api post

--- src/components/userAuthentication/Registration.js
+++ src/components/userAuthentication/Registration.js
@@ -19,15 +19,16 @@ const RegForm = ({values, errors, touched, status}) => {
         {touched.email && errors.email && (<p className="errors">{errors.email}</p>)}
         
         <Field type="text" name="username" placeholder="username" />
-        {touched.username && errors.username && (<p className="errors">{errors.username}</p>)}
+        {touched.username && errors.username && (<p className="errors">user name is a required field</p>)}
         
         <Field type="password" name="password" placeholder="password" />
         <p>*Password must be between 6 and 12 characters in length.</p>
         {touched.password && errors.password && (<p className="errors">{errors.password}</p>)}
-{/*         
+        
         <Field type="password" name="passwordVerify" placeholder="confirm password" />
-        {touched.passwordVerify && errors.passwordVerify && (<p className="errors">"You must confirm you password!"</p>)}        
-         */}
+        {touched.passwordVerify && errors.passwordVerify} 
+        {/* && (<p className="errors">"You must confirm your password!"</p>)} */}
+
         <Field type="text" name="name" placeholder="name" />
         {touched.name && errors.name && (<p className="errors">{errors.name}</p>)}        
         
@@ -36,25 +37,22 @@ const RegForm = ({values, errors, touched, status}) => {
             Yes! I would like to receive future promotions and FoodTruck TrackR location updates via text message!</label>
         <Field type="checkbox" name="consent" checked={values.consent}/> */}
         </div>
+        
         <div>
         <label className="role">
-            I would like to register as a FoodTruck TrackR </label>
+          I would like to register as a FoodTruck TrackR </label>
         <Field as="select" name="role">
-        <option>...select</option>
-        <option value="diner">Diner</option>
-        <option value="operator">Operator</option>
-        {touched.role && errors.role && (<p className="errors">{errors.role}</p>)}   
+          <option hidden={true}>...select</option>
+          <option disabled="disabled" default={true}>...select</option>
+          <option value="diner">Diner</option>
+          <option value="operator">Operator</option>
+          {touched.role && errors.role && (<p className="errors">{errors.role}</p>)}   
         </Field>
         </div>
 
         <button type="submit">Register</button>
       
       </Form>
-
-      {/* {user.map(person => (
-        key={person.id};
-        name={person.userName};
-      ))} */}
     </div>
 
   )//closes RegForm return
@@ -66,20 +64,16 @@ const FormikRegistration = withFormik({
     username, 
     email, 
     password, 
-    // passwordVerify, 
+    passwordVerify, 
     name,
-    // phone, 
-    // consent, 
     role}) 
     {
     return{
       username: username || "",
       email: email || "",
       password: password || "",
-      // passwordVerify: passwordVerify || "",
+      passwordVerify: passwordVerify || "",
       name: name || "",
-      //phone: phone || "",
-      // consent: consent || false,
       role: role || false
     };
   },
@@ -87,39 +81,44 @@ const FormikRegistration = withFormik({
   validationSchema: yup.object().shape({
     email: yup
       .string()
-      .label("email")
       .email()
       .required(),
     username: yup
       .string()
-      .label("userName")
       .required(),
     password: yup
       .string()
-      .label("password")
       .required("Password is required")
       .min(6, "Password must be at least 6 characters long.")
       .max(10, "Password must not exceed 12 characters."),
-    // passwordVerify: yup
-      // .string()
-      // .label("passwordVerify")
-      // .oneOf([yup.ref("password"), null], "Passwords must match!"),
-    name: yup
+    passwordVerify: yup
       .string()
-      .label("name")
+      .required("Must confirm Password!")
+      .oneOf([yup.ref("password"), null], "Passwords must match!"),
+    
+      name: yup
+      .string()
       .required("Name is required"),
-    // consent: yup
-    //   .boolean(),
-      //needs .required if phone number has been entered
     role: yup
       .string()
       .required(),
   }),
 
   handleSubmit(values, {setStatus}) {
+    
+    const person = {
+      username: values.username,
+      password: values.password,
+      role: values.role,
+      name: values.name, 
+      email: values.email}
+
+      console.log(person)
+      
     axios
-    .post("https://food-truck-trakr.herokuapp.com/api/register", values)
+    .post("https://food-truck-trakr.herokuapp.com/api/register", person)
     .then(response => {
+      console.log(person);
       console.log(response.data, "user values");
       setStatus(response.data);
       //if operator.value =true && email/username is unique
